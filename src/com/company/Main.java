df33813f60d14970b84a08a61f1fb11644f7a978
Z Chen <zchen6464@bths.edu>
2018-12-17 11:40:57 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Ticket x = new
+        Ticket temp = new Advance( 3);
+        Ticket temp2 = new StudentAdvance(3);
+        Ticket temp3 = new Walkup();
+        Ticket temp4 = new Advance( 11);
+        Ticket temp5 = new StudentAdvance(11);
+
     }
 }
